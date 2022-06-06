97e6823d591b3f44e3fe7171465d6fc7556e81ae
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-06-06 13:26:26 -0300

    Updates

---
+++ precache-manifest.ee24a0e94f5d8b8dcbf1ffeeabaf1d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ef2fec523bb0480aec0da39794d952",
+    "url": "/front-end-online-store/index.html"
+  },
+  {
+    "revision": "1fd7641106b2c79bb537",
+    "url": "/front-end-online-store/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2c7fdca5f58f5c793ca",
+    "url": "/front-end-online-store/static/js/2.7ab0942d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/front-end-online-store/static/js/2.7ab0942d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd7641106b2c79bb537",
+    "url": "/front-end-online-store/static/js/main.f46a8f46.chunk.js"
+  },
+  {
+    "revision": "9fd8b18ceee1540cab08",
+    "url": "/front-end-online-store/static/js/runtime-main.0d3c3230.js"
+  }
+]);
